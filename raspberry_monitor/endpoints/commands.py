7e0bf173b7ee859e2a4092ef1d969404dd759a8d
Rodolfo Cugler <rodolfo.cugler@hexad.de>
2022-09-16 22:42:17 +0200

    Fixing request issue

--- raspberry_monitor/endpoints/commands.py
+++ raspberry_monitor/endpoints/commands.py
@@ -1,7 +1,7 @@
 import logging
 from http import HTTPStatus
 
-from flask_restx import Namespace, Resource, abort
+from flask_restx import Namespace, Resource, abort, reqparse
 
 from raspberry_monitor import config, oidc
 from raspberry_monitor.services import power_mode
@@ -10,22 +10,28 @@ api = Namespace("commands")
 
 conf = config.Config()
 
+_post_parser = reqparse.RequestParser()
+_post_parser.add_argument("command_id", type=str, required=True, help="Command Id", default="restart")
+
 
 @api.route("/commands")
 class Command(Resource):
 
     @api.doc("/",
-             params={"command_id": "Command id"},
              responses={
                  HTTPStatus.UNAUTHORIZED: "Request unauthorized",
                  HTTPStatus.OK: "Command successful executed"
              },
              security="auth")
+    @api.expect(_post_parser)
     @oidc.accept_token(require_token=conf.OIDC_ENABLED)
     @oidc.require_keycloak_role(require_token=conf.OIDC_ENABLED, client=conf.OIDC_CLIENT,
                                 roles=["admin", "uma_protection"],
                                 all_roles_required=False)
-    def post(self, command_id):
+    def post(self):
+        args = _post_parser.parse_args()
+        command_id = args["command_id"]
+
         logging.debug(f"Executing {command_id}")
         if command_id == "restart":
             power_mode.restart()
