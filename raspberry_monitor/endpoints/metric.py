7613ccfedbad5530e81e86196a1f87ef417acfe8
Rodolfo Cugler <rodolfocugler@outlook.com>
2023-12-04 15:01:41 +0100

    Update metric.py

--- raspberry_monitor/endpoints/metric.py
+++ raspberry_monitor/endpoints/metric.py
@@ -3,12 +3,9 @@ from http import HTTPStatus
 
 from flask_restx import Namespace, Resource
 
-from raspberry_monitor import config, oidc
 from raspberry_monitor.services import metric_read
 
 api = Namespace("metrics")
-
-conf = config.Config()
 
 
 @api.route("/metrics")
@@ -16,14 +13,8 @@ class Metric(Resource):
 
     @api.doc("/",
              responses={
-                 HTTPStatus.UNAUTHORIZED: "Request unauthorized",
                  HTTPStatus.OK: "Metrics"
-             },
-             security="auth")
-    @oidc.accept_token(require_token=conf.OIDC_ENABLED)
-    @oidc.require_keycloak_role(require_token=conf.OIDC_ENABLED, client=conf.OIDC_CLIENT,
-                                roles=["admin", "uma_protection"],
-                                all_roles_required=False)
+             })
     def get(self):
         logging.debug(f"get metrics")
         metrics = metric_read.get()
